25efbfd57089cfdaa1f1776f06f619be544bf511
Elio Heredia <elio22arg@gmail.com>
2023-03-19 23:37:21 -0300

    cambios-firebase

--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3_nYiLhzwn3Dpp7iulTg5H_kjk3gD2b8",
+  authDomain: "homestore---website.firebaseapp.com",
+  projectId: "homestore---website",
+  storageBucket: "homestore---website.appspot.com",
+  messagingSenderId: "874486633104",
+  appId: "1:874486633104:web:c73183d79bbe6e9a0e3e15"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
 root.render(
     <App />
   
--- src/pages/ItemListContainer/ItemListContainer.js
+++ src/pages/ItemListContainer/ItemListContainer.js
@@ -2,39 +2,52 @@ import { stockProducts } from "../../data/stockProducts";
 import { useEffect, useState } from "react";
 import ItemList from "../../components/ItemList/ItemList";
 import { useParams } from "react-router-dom";
+import { getFirestore, getDocs, collection } from "firebase/firestore";
 
 const ItemListContainer = ({ greeting }) => {
   const [productList, setProductList] = useState([]);
-  const { categoryId } = useParams () ;
-  const getProducts = new Promise((res, rej) => {
-    
-    if (categoryId) {
-      const filtroProd = stockProducts.filter(
-        (item) => item.category === categoryId
-        );
-    setTimeout(() => {
-      res(filtroProd);
-    }, 1000);
-  } else {
-    setTimeout (() => {
-      res(stockProducts);
-    }, 1000);
-  }
+  const { categoryId } = useParams();
+  // const getProducts = new Promise((res, rej) => {
 
-  });
+  //   if (categoryId) {
+  //     const filtroProd = stockProducts.filter(
+  //       (item) => item.category === categoryId
+  //     );
+  //     setTimeout(() => {
+  //       res(filtroProd);
+  //     }, 1000);
+  //   } else {
+  //     setTimeout(() => {
+  //       res(stockProducts);
+  //     }, 1000);
+  //   }
 
-  useEffect (() => {
-    getProducts
-      .then ((response) => { 
-        ///console.log(response);
-        setProductList (response);
-      })
-      .catch((error) => {
-      console.log(error);
-    });
+  // });
+
+  const getProducts = () => {
+    const db = getFirestore();
+    const querySnapshot = collection(db, 'productos');
+
+    getDocs(querySnapshot)
+      .then((response) => {
+
+    })
+      .catch((error) => console.log(error))
+  };
+
+
+  useEffect(() => {
+    getProducts ();
+
+      // .then((response) => {
+      //   setProductList(response);
+      // })
+      // .catch((error) => {
+      //   console.log(error);
+      // });
   }, [categoryId]);
 
-  return  (
+  return (
     <div>
       <ItemList productList={productList} />
     </div>
